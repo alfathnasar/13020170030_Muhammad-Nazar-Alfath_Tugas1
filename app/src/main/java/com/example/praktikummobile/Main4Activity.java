80c00d41bd0a0576dbdb6a0902925277d4eda0e2
Alfathnasar <alfathnasar@gmail.com>
2020-05-11 21:47:30 +0800

    First Commit

---
+++ app/src/main/java/com/example/praktikummobile/Main4Activity.java
@@ -0,0 +1,18 @@
+package com.example.praktikummobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        Button button = findViewById(R.id.button);
+    }
+}
